bbe8f81b66ff25e83c400cc4eeb8c247736ecae0
Vijeth <vijethpoojaryy@gmail.com>
2026-02-13 20:29:52 +0530

    Adding animations ,gradient , glassmorphism ,  typing effect

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function showMessage() {
-    alert("You clicked the button! 🚀");
+    alert("Welcome to the future 🚀");
 }
 
+/* Typing Effect */
+const text = "Hello Hackathon 🚀";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.querySelector(".typing").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 80);
+    }
+}
+
+window.onload = typeEffect;
+
+/* Dark/Light Mode */
 function toggleMode() {
     document.body.classList.toggle("light-mode");
 }
